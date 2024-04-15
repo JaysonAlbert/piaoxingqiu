af125036b039f9ea9785dd2fb5a2026d0c4fc212
wangjie <momeijw@gmail.com>
2024-04-15 19:22:52 +0800

    add logo and title

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         supportedLocales: AppLocalizations.supportedLocales,
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         ),
         home: LoginPage());
   }
@@ -43,30 +43,65 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     var localizations = AppLocalizations.of(context);
+    double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
-        appBar: AppBar(title: Center(child: Text(localizations!.appTitle))),
-        body: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                TextField(
-                    controller: _username,
-                    decoration: InputDecoration(
-                      labelText: localizations.username,
-                      border: OutlineInputBorder(),
-                    )),
-                const SizedBox(height: 20),
-                TextField(
-                    controller: _password,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      labelText: localizations.password,
-                      border: OutlineInputBorder(),
-                    )),
-                const SizedBox(height: 20),
-                ElevatedButton(onPressed: _login, child: Text('login')),
-              ],
-            )));
+        body: Stack(children: [
+      Center(
+        child: Column(
+          children: [
+            Image.network(
+              'https://cdn.ticketmars.com/image/prod/20220617_27/16554485546192345.png',
+              height: screenHeight / 6,
+            ),
+            Text(localizations!.appTitle,
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold))
+          ],
+        ),
+      ),
+      Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextField(
+                  controller: _username,
+                  keyboardType: TextInputType.phone,
+                  decoration: InputDecoration(
+                    labelText: localizations.username,
+                    border: OutlineInputBorder(),
+                  )),
+              SizedBox(height: 16),
+              TextField(
+                  controller: _password,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: localizations.password,
+                    border: OutlineInputBorder(),
+                    suffixIcon: TextButton(
+                      onPressed: () {},
+                      style: TextButton.styleFrom(
+                        iconColor: Theme.of(context).primaryColor,
+                      ),
+                      child: Text(localizations.sendSms),
+                    ),
+                  )),
+              SizedBox(height: 16),
+              Center(
+                  child: Row(
+                children: [
+                  Expanded(
+                      child: ElevatedButton(
+                          onPressed: _login,
+                          style: ElevatedButton.styleFrom(
+                              minimumSize: Size.fromHeight(50)),
+                          child: Text(
+                            localizations.login,
+                          )))
+                ],
+              )),
+            ],
+          ))
+    ]));
   }
 }

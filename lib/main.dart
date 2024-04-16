cbe31148df3f2ed1d663bd0e6db606bae75a3cb0
wangjie <momeijw@gmail.com>
2024-04-16 12:31:43 +0800

    add go_router support

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,21 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:piaoxingqiu/models/user.dart';
 import 'dart:core';
 import 'package:provider/provider.dart';
+import 'package:go_router/go_router.dart';
 
 import 'views/login_screen.dart';
+
+final _router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => ChangeNotifierProvider(
+        create: (context) => UserModel(),
+        child: const LoginPage(),
+      ),
+    ),
+  ],
+);
 
 void main() {
   runApp(const MyApp());
@@ -16,17 +29,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        localizationsDelegates: AppLocalizations.localizationsDelegates,
-        supportedLocales: AppLocalizations.supportedLocales,
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
-        ),
-        home: ChangeNotifierProvider(
-          create: (context) => UserModel(),
-          child: const LoginPage(),
-        ));
+    return MaterialApp.router(
+      routerConfig: _router,
+      title: 'Flutter Demo',
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
+      ),
+    );
   }
 }

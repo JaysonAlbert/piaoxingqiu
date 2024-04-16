d769fcaaa4fa7c376f654995eb59aa12354e38ea
Jayson Albert <momeijw@gmail.com>
2024-04-17 01:56:36 +0800

    shows screen

--- lib/widgets/show_card_widget.dart
+++ lib/widgets/show_card_widget.dart
@@ -1,10 +1,82 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class ShowCardWidget extends StatelessWidget {
-  const ShowCardWidget({super.key});
+  ShowCardWidget({Key? key, required this.showData}) : super(key: key);
+
+  final Map<String, dynamic> showData;
+
+  String _price() {
+    final priceInfo = showData['minOriginalPriceInfo']! as Map<String, String>;
+    return '${priceInfo['prefix']}${priceInfo['yuanNum']}';
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 5),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(15),
+              child: CachedNetworkImage(
+                height: 100,
+                width: 77,
+                imageUrl: showData['posterUrl'] as String,
+                placeholder: (context, url) => CircularProgressIndicator(),
+                errorWidget: (context, url, error) => Icon(Icons.error),
+              ),
+            ),
+            IntrinsicWidth(
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text(
+                          showData['showName'] as String,
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          showData['showDate'] as String,
+                          style: TextStyle(color: Colors.grey, fontSize: 12),
+                        ),
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                                '${showData['cityName'] as String}|${showData['venueName'] as String}',
+                                style: TextStyle(
+                                    color: Colors.grey, fontSize: 12)),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            _price(),
+                            style: TextStyle(
+                                color: Colors.red, fontWeight: FontWeight.bold),
+                          ),
+                          Text('起')
+                        ])
+                  ]),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
